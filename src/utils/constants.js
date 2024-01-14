967d01f26ba1c449f28a55d9728bf23ef61fd832
Siddharth Sharma <vsssiddharth@gmail.com>
2024-01-14 09:12:50 +0530

    env file applied

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,7 @@
-export const API_KEY = "AIzaSyDaYOvtKdmH5SKCKnQZ4uBzuJKGmvzu0jE"
 
+export const API_KEY = import.meta.env.VITE_API_KEY
 export const FETCH_URL = " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+API_KEY
-//AIzaSyCICLQm76jeU8Fjx1jgzJx_wKC-0407G0M
-// AIzaSyDaYOvtKdmH5SKCKnQZ4uBzuJKGmvzu0jE
+
 export const AUTO_SUGGEST_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const LIVE_CHAT_COUNT = 30 //calculate according to device capabality
